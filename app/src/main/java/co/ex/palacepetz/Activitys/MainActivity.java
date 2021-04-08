5541feb15b82cd42fbca9e4541670ee824e79e44
kaua vitorio <kauavitorioof@gmail.com>
2021-04-07 21:35:06 -0300

    first commit

---
+++ app/src/main/java/co/ex/palacepetz/Activitys/MainActivity.java
@@ -0,0 +1,16 @@
+package co.ex.palacepetz.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import co.ex.palacepetz.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
