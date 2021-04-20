286a6d5d5f1cbe8214e967669cddc7e4f23e04f2
kaua vitorio <kauavitorioof@gmail.com>
2021-04-19 23:40:07 -0300

    Create AllProducts and Details (#17)

--- app/src/main/java/com/kaua/palacepetz/Activitys/RegisterAddressActivity.java
+++ app/src/main/java/com/kaua/palacepetz/Activitys/RegisterAddressActivity.java
@@ -2,9 +2,9 @@ package com.kaua.palacepetz.Activitys;
 
 import androidx.fragment.app.FragmentActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,16 +16,26 @@ import com.kaua.palacepetz.R;
 public class RegisterAddressActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LottieAnimationView arrowGoBackRegisterAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_address);
+        Ids();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map_register_address);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
+
+        arrowGoBackRegisterAddress.setOnClickListener(v -> {
+            finish();
+        });
+    }
+
+    private void Ids(){
+        arrowGoBackRegisterAddress = findViewById(R.id.arrowGoBackRegisterAddress);
     }
 
     @Override
@@ -39,22 +49,6 @@ public class RegisterAddressActivity extends FragmentActivity implements OnMapRe
     }
 
     private void GoBack_toMain() {
-        Intent GoBack_ToMain = new Intent(RegisterAddressActivity.this, MainActivity.class);
-        /*GoBack_ToMain.putExtra("id_user", id_user);
-        GoBack_ToMain.putExtra("nm_user", nm_user);
-        GoBack_ToMain.putExtra("email_user", email_user);
-        GoBack_ToMain.putExtra("phone_user", phone_user);
-        GoBack_ToMain.putExtra("zipcode", zipcode);
-        GoBack_ToMain.putExtra("address_user", address_user);
-        GoBack_ToMain.putExtra("complement", complement);
-        GoBack_ToMain.putExtra("img_user", img_user);
-        GoBack_ToMain.putExtra("address_user", address_user);
-        GoBack_ToMain.putExtra("cpf_user", cpf_user);
-        GoBack_ToMain.putExtra("partner", partner);
-        GoBack_ToMain.putExtra("partner_Startdate", partner_Startdate);
-        GoBack_ToMain.putExtra("statusavisoend", "desativado");*/
-        startActivity(GoBack_ToMain);
-        //StopConexaoWithGoogleApi();
         finish();
     }
 
