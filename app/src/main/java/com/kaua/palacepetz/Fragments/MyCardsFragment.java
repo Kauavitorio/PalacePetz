086d83d437af1daabb130ba564092f9561ec7502
kaua vitorio <kauavitorioof@gmail.com>
2021-04-24 23:01:38 -0300

    Create CardRegistration (#31)

--- app/src/main/java/com/kaua/palacepetz/Fragments/MyCardsFragment.java
+++ app/src/main/java/com/kaua/palacepetz/Fragments/MyCardsFragment.java
@@ -10,22 +10,28 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.kaua.palacepetz.Activitys.MainActivity;
 import com.kaua.palacepetz.Adapters.IOnBackPressed;
 import com.kaua.palacepetz.R;
 
+import java.util.Objects;
+
 public class MyCardsFragment extends Fragment implements IOnBackPressed {
     //  Screen Items
     private RecyclerView recyclerView_Cards;
     private CardView cardContainer_AddCard, BtnMyCard_AddCard, BtnMyCard_AddNewCard;
+    private static FragmentTransaction transaction;
+
+
 
     //  Fragments Arguments
     Bundle args;
 
     //  User information
-    String email_user;
+    String _Email;
 
     @Nullable
     @Override
@@ -34,22 +40,38 @@ public class MyCardsFragment extends Fragment implements IOnBackPressed {
         Ids(view);
         cardContainer_AddCard.setElevation(19);
         BtnMyCard_AddCard.setElevation(20);
+        BtnMyCard_AddNewCard.setElevation(20);
 
         args = getArguments();
         assert args != null;
-        email_user = args.getString("email_user");
+        _Email = args.getString("email_user");
 
         MainActivity activity = (MainActivity) getActivity();
         assert activity != null;
         activity.updateCart();
 
-        BtnMyCard_AddCard.setOnClickListener(v1 -> {
+        BtnMyCard_AddCard.setOnClickListener(v -> {
             cardContainer_AddCard.setElevation(0);
             BtnMyCard_AddCard.setElevation(0);
+            Goto_CardRegistration();
+        });
 
+        BtnMyCard_AddNewCard.setOnClickListener(v -> {
+            BtnMyCard_AddNewCard.setElevation(0);
+            Goto_CardRegistration();
         });
 
         return view;
+    }
+
+    private void Goto_CardRegistration() {
+        CardRegistrationFragment cardregistrationFragment = new CardRegistrationFragment();
+        args = new Bundle();
+        args.putString("email_user", _Email);
+        cardregistrationFragment.setArguments(args);
+        transaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayoutMain, cardregistrationFragment);
+        transaction.commit();
     }
 
     private void Ids(View view) {
@@ -61,7 +83,7 @@ public class MyCardsFragment extends Fragment implements IOnBackPressed {
 
     @Override
     public boolean onBackPressed() {
-        if (email_user != null) {
+        if (_Email != null) {
             //action not popBackStack
             return true;
         } else {
