023e0860e15f125a37fd3895e560d997087707d7
kaua vitorio <kauavitorioof@gmail.com>
2021-04-25 12:36:47 -0300

    Set Create request for cads and adapter layout (#32)

--- app/src/main/java/com/kaua/palacepetz/Fragments/MyCardsFragment.java
+++ app/src/main/java/com/kaua/palacepetz/Fragments/MyCardsFragment.java
@@ -9,29 +9,53 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.kaua.palacepetz.Activitys.MainActivity;
 import com.kaua.palacepetz.Adapters.IOnBackPressed;
+import com.kaua.palacepetz.Adapters.LoadingDialog;
+import com.kaua.palacepetz.Data.Cards.AsyncCards;
+import com.kaua.palacepetz.Data.Cards.CardService;
+import com.kaua.palacepetz.Data.Cards.DtoCard;
 import com.kaua.palacepetz.R;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+@SuppressWarnings("unchecked")
 public class MyCardsFragment extends Fragment implements IOnBackPressed {
     //  Screen Items
     private RecyclerView recyclerView_Cards;
     private CardView cardContainer_AddCard, BtnMyCard_AddCard, BtnMyCard_AddNewCard;
+    private ConstraintLayout container_noCard;
     private static FragmentTransaction transaction;
-
-
+    private LoadingDialog loadingDialog;
 
     //  Fragments Arguments
     Bundle args;
 
     //  User information
     String _Email;
+
+    //  Retrofit
+    String baseurl = "https://coffeeforcode.herokuapp.com/";
+    final Retrofit retrofitCard = new Retrofit.Builder()
+            .baseUrl( baseurl + "card/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
 
     @Nullable
     @Override
@@ -45,6 +69,7 @@ public class MyCardsFragment extends Fragment implements IOnBackPressed {
         args = getArguments();
         assert args != null;
         _Email = args.getString("email_user");
+        getCardsInformation();
 
         MainActivity activity = (MainActivity) getActivity();
         assert activity != null;
@@ -64,6 +89,39 @@ public class MyCardsFragment extends Fragment implements IOnBackPressed {
         return view;
     }
 
+
+    private void getCardsInformation() {
+        loadingDialog.startLoading();
+        CardService cardService = retrofitCard.create(CardService.class);
+        Call<DtoCard> cardCall = cardService.getCardsOfUser(_Email);
+        cardCall.enqueue(new Callback<DtoCard>() {
+            @Override
+            public void onResponse(@NotNull Call<DtoCard> call, @NotNull Response<DtoCard> response) {
+                loadingDialog.dimissDialog();
+                if (response.code() == 412){
+                    container_noCard.setVisibility(View.VISIBLE);
+                    recyclerView_Cards.setVisibility(View.GONE);
+                }else if(response.code() == 200){
+                    assert response.body() != null;
+                    LinearLayoutManager linearLayout = new LinearLayoutManager(getActivity());
+                    recyclerView_Cards.setLayoutManager(linearLayout);
+                    AsyncCards asyncCards = new AsyncCards(recyclerView_Cards, container_noCard, getActivity(), _Email);
+                    asyncCards.execute();
+                    if (response.body().getLength() < 3){
+                        BtnMyCard_AddNewCard.setVisibility(View.VISIBLE);
+                    }else{
+                        BtnMyCard_AddNewCard.setVisibility(View.GONE);
+                    }
+                }
+            }
+            @Override
+            public void onFailure(@NotNull Call<DtoCard> call, @NotNull Throwable t) {
+                loadingDialog.dimissDialog();
+                Toast.makeText(getActivity(), R.string.weHaveAProblem, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     private void Goto_CardRegistration() {
         CardRegistrationFragment cardregistrationFragment = new CardRegistrationFragment();
         args = new Bundle();
@@ -79,6 +137,8 @@ public class MyCardsFragment extends Fragment implements IOnBackPressed {
         cardContainer_AddCard = view.findViewById(R.id.cardContainer_AddCard);
         BtnMyCard_AddCard = view.findViewById(R.id.BtnMyCard_AddCard);
         BtnMyCard_AddNewCard = view.findViewById(R.id.BtnMyCard_AddNewCard);
+        container_noCard = view.findViewById(R.id.container_noCard);
+        loadingDialog = new LoadingDialog(getActivity());
     }
 
     @Override
