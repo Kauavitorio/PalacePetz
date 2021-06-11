0f69f295f991216b09a2089c81ebbf80e9733b57
kaua vitorio <kauavitorioof@gmail.com>
2021-06-10 22:39:33 -0300

    Start New Layout (#57)

---
+++ app/src/main/java/co/kaua/palacepetz/Activitys/IntroActivity.java
@@ -0,0 +1,16 @@
+package co.kaua.palacepetz.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import co.kaua.palacepetz.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
